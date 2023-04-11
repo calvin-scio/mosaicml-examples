d21349b47d7ae2a5b4de4910e103a2fe1cf07ef9
Calvin Qi <calvinqi@Calvins-MacBook-Pro-2.local>
2023-04-11 11:27:14 -0700

    test

--- examples/llm/test_inference.py
+++ examples/llm/test_inference.py
@@ -1,18 +1,11 @@
-from transformers import AutoTokenizer, GPT2Config, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM
 from transformers import AutoConfig
 
 config = AutoConfig.from_pretrained('EleutherAI/gpt-neo-125m')
 
-# CHECKPOINT_DIR = "./run125/checkpoints/latest-rank0.pt"
+CHECKPOINT_DIR = './run125/hf_export'
 
-model = AutoModelForCausalLM.from_pretrained('./run125/checkpoints/latest-rank0.pt', config=config)
-
-
-from transformers import AutoTokenizer
-from optimum.onnxruntime import ORTModelForCausalLM
-
-model = ORTModelForCausalLM.from_pretrained("./run125/", config=config, from_transformers=True)
-tokenizer = AutoTokenizer.from_pretrained("gpt2")
+model = AutoModelForCausalLM.from_pretrained(CHECKPOINT_DIR, config=config)
 
 import code
 code.interact(local=locals())
