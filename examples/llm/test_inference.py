7a4a04a1a36d9b70ee615efefcd130a2b9d54d4e
Calvin Qi <calvinqi@Calvins-MacBook-Pro-2.local>
2023-04-11 08:43:16 -0700

    test

--- examples/llm/test_inference.py
+++ examples/llm/test_inference.py
@@ -3,5 +3,13 @@ from transformers import AutoTokenizer, GPT2Model
 CHECKPOINT_DIR = "./run125/checkpoints/latest-rank0.pt"
 
 model = GPT2Model.from_pretrained(CHECKPOINT_DIR)
+
+
+from transformers import AutoTokenizer
+from optimum.onnxruntime import ORTModelForCausalLM
+
+model = ORTModelForCausalLM.from_pretrained("./run125/model_export.onnx",from_transformers=True)
+tokenizer = AutoTokenizer.from_pretrained("gpt2")
+
 import code
 code.interact(local=locals())
