0ffc3356ddfc6124a884cf2d30c227ed142035db
Calvin Qi <calvinqi@Calvins-MacBook-Pro-2.local>
2023-04-11 08:22:26 -0700

    test

--- examples/llm/test_inference.py
+++ examples/llm/test_inference.py
@@ -1,7 +1,7 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, GPT2Model
 
 CHECKPOINT_DIR = "./run125/checkpoints/latest-rank0.pt"
 
-model = AutoModelForCausalLM.from_pretrained(CHECKPOINT_DIR)
+model = GPT2Model.from_pretrained(CHECKPOINT_DIR)
 import code
 code.interact(local=locals())
