6058056de41444bebe43faac8a51cce6a68eccd6
Calvin Qi <calvinqi@Calvins-MacBook-Pro-2.local>
2023-04-11 13:07:25 -0700

    test

---
+++ examples/llm/convert_data.py
@@ -0,0 +1,28 @@
+# Based on https://github.com/mosaicml/streaming
+import argparse
+import numpy as np
+from PIL import Image
+from streaming import MDSWriter
+import json
+
+
+# A dictionary mapping input fields to their data types
+columns = {
+    'text': 'str'
+}
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_data_dir', type=str, required=True, help='data directory')  
+    parser.add_argument('--output_data_dir', type=str, required=True, help='data directory')  
+    args = parser.parse_args()
+
+    # Save the samples as shards using MDSWriter
+    with MDSWriter(out=args.output_data_dir, columns=columns) as out:
+        with open(args.input_data_dir, 'r') as f:
+            for line in f:
+                sample = json.loads(line.strip())
+                out.write(sample)
+
+if __name__ == '__main__':
+    main()
